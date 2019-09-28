4cbe745b4cdbd2766d1e0ee78c20134cb0ef5da1
willook <willook@naver.com>
2019-09-28 11:13:39 +0900

    Change module to tf.keras

--- models/UNet.py
+++ models/UNet.py
@@ -1,26 +1,32 @@
 import os,time,cv2
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+#import tensorflow.contrib.slim as slim
 import numpy as np
 
 def ConvBlock(inputs, n_filters, kernel_size=[3, 3]):
     """
     Builds the conv block for UNets
-    Apply successivly a 2D convolution, BatchNormalization relu
+    Apply successivly a 2D convolution, BatchNormalization, ReLU
     """
-    net = slim.conv2d(inputs, n_filters, kernel_size=kernel_size, activation_fn=None)
-    net = slim.batch_norm(net, fused=True)
-    net = tf.nn.relu(net)
+    conv_layer = tf.keras.layers.Conv2D(n_filters, kernel_size=kernel_size, padding='same')
+    batch_norm = tf.keras.layers.BatchNormalization(axis = -1, fused=True)
+    
+    net = conv_layer(inputs)
+    net = tf.keras.activations.relu(net)
+    net = batch_norm(net)
     return net
 
 def conv_transpose_block(inputs, n_filters, kernel_size=[3, 3]):
     """
     Basic conv transpose block for Encoder-Decoder upsampling
-    Apply successivly Transposed Convolution, BatchNormalization, ReLU nonlinearity
+    Apply successivly Transposed Convolution, BatchNormalization, ReLU
     """
-    net = slim.conv2d_transpose(inputs, n_filters, kernel_size=kernel_size, stride=[2, 2], activation_fn=None)
-    net = slim.batch_norm(net)
-    net = tf.nn.relu(net)
+    conv2d_transpose_layer = tf.keras.layers.Conv2DTranspose(n_filters, kernel_size=kernel_size, strides=[2, 2], padding="same")
+    batch_norm = tf.keras.layers.BatchNormalization(axis = -1, fused=True)
+
+    net = conv2d_transpose_layer(inputs)
+    net = tf.keras.activations.relu(net)
+    net = batch_norm(net)
     return net
 
 def build_unet(inputs, preset_model, num_classes):
@@ -28,27 +34,29 @@ def build_unet(inputs, preset_model, num_classes):
     #####################
     # Downsampling path #
     #####################
-
+    
+    max_pooling = tf.keras.layers.MaxPool2D(pool_size = (2,2), strides = (2,2), padding = "same")
+    
     net = ConvBlock(inputs, 64)
     net = ConvBlock(net, 64)
     skip_1 = net
 
-    net = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
+    net = max_pooling(net)
     net = ConvBlock(net, 128)
     net = ConvBlock(net, 128)
     skip_2 = net
-
-    net = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
+    
+    net = max_pooling(net)
     net = ConvBlock(net, 256)
     net = ConvBlock(net, 256)
     skip_3 = net
 
-    net = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
+    net = max_pooling(net)
     net = ConvBlock(net, 512)
     net = ConvBlock(net, 512)
     skip_4 = net
 
-    net = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
+    net = max_pooling(net)
     net = ConvBlock(net, 1024)
     net = ConvBlock(net, 1024)
 
@@ -79,5 +87,7 @@ def build_unet(inputs, preset_model, num_classes):
     #####################
     #      Softmax      #
     #####################
-    net = slim.conv2d(net, num_classes, [1, 1], activation_fn=None, scope='logits')
+    
+    fc_layer = tf.keras.layers.Conv2D(num_classes, kernel_size=[1, 1], padding='same')
+    net = fc_layer(net)
     return net
